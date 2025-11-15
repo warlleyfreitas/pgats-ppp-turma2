641903068a1d48852111b1829e270e4bc766fc11
warlleyfreitas <warlley.freitas@gmail.com>
2025-11-15 11:19:14 -0300

    Atualizando script gerado

--- tests/k6/primeiro-script-gerado.js
+++ tests/k6/primeiro-script-gerado.js
@@ -1,15 +1,22 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-import { expect } from "https://jslib.k6.io/k6-testing/0.5.0/index.js";
+import { expect } from 'https://jslib.k6.io/k6-expect/0.0.2/index.js';
+import { sleep, check } from 'k6';
 
 export const options = {
   vus: 10,
   duration: '30s',
 };
 
-export default function() {
+export default function () {
   let res = http.get('https://quickpizza.grafana.com');
-  expect.soft(res.status).toBe(200);
-  sleep(1);
 
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'status text is OK': (r) => r.status_text === 'OK',
+  });
+
+  expect.soft(res.status).toBe(200);
+  expect.soft(res.status_text).toBe('OK');
+
+  sleep(1);
 }
